902fe22088671f9de244ee591ed2503de43201d4
waymg <wayilgmustafa@gmail.com>
2020-09-09 12:37:21 -0400

    .

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function () {
+  searchCovid()
+  $("#search-button").on("click", function () {
+    var searchValue = $("#search-value").val();
+
+    // clear input box
+    $("#search-value").val("");
+    //Search Value is the City name
+    searchCovid(searchValue);
+  });
+
+  function searchCovid() {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/us/current.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }})
