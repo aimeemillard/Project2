e298c0bfabeb7cce36104c6bc41e7dc5399bd05e
waymg <wayilgmustafa@gmail.com>
2020-09-08 23:46:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,98 @@ $(document).ready(function () {
       type: "GET",
       url:
         "https://api.covidtracking.com" +
-        "/v1/us/current.json",
+        "/v1/us/current.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/us/daily.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/states/info.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/states/{state}/info.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/states/{state}/current.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/states/{state}/daily.json", 
+
+        
+      dataType: "json", 
+      success: function (data) {
+      console.log(data)
+       
+      }
+    });
+  }
+  function searchCovid(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.covidtracking.com" +
+        "/v1/states/{state}/{date}.json", 
+
         
       dataType: "json", 
       success: function (data) {
